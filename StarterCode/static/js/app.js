46422c8f81621c1e106a7a72906334cbf79c5b2c
Kirsteen Lugtu <klugtu@Kirsteens-MacBook-Pro.local>
2021-12-06 22:55:12 -0600

    Added additional filters to index.html, added functioning date filter to app.js .

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,44 +2,54 @@ console.log("app.js loaded")
 
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // Select button and form
 let button = d3.select("#filter-btn");
 let form = d3.select("#form");
 
 // Create event handlers for pressing enter key or clicking Filter button
-// button.on("click", runFilter);
-// form.on("submit", runFilter);
+button.on("click", runFilter);
+form.on("submit", runFilter);
 
-// ------ Filter out data ---------------
-// function runFilter(){
 
-// };
+function runFilter(){
 
-// // let dateIn = d3.select("#datetime");
-// let filteredData = tableData.filter(sighting => sighting.datetime === dateIn);
+    d3.event.preventDefault();
 
-// ------ Fill in table with selected data ------
+    // ------ Filter out data ---------------
+    let inputElement = d3.select("#datetime");
 
-tbody = d3.select("tbody");
+    let dateIn = inputElement.property("value");
 
-// filteredData.forEach(function(sighting){
-tableData.forEach(function(sighting){
-    console.log(sighting);
-    
-    // Add a new row for each ufo sighting
-    let row = tbody.append("tr");
+    console.log(dateIn);
 
-    // Loop through key-value pairs
-    Object.entries(sighting).forEach(function([key, value]) {
+    let filteredData = tableData.filter(sighting => sighting.datetime === dateIn);
 
-        // console.log(`Key ${key} Value ${value}`)
+    console.log(filteredData);
 
-        // add table data element
-        let cell = row.append("td");
 
-        // assign each cell value in table data element
-        cell.text(value);
+    // ------ Fill in table with selected data ------
+
+    tbody = d3.select("tbody");
+
+    filteredData.forEach(function(sighting){
+
+        console.log(sighting);
+        
+        // Add a new row for each ufo sighting
+        let row = tbody.append("tr");
+
+        // Loop through key-value pairs
+        Object.entries(sighting).forEach(function([key, value]) {
+
+            // console.log(`Key ${key} Value ${value}`)
+
+            // add table data element
+            let cell = row.append("td");
+
+            // assign each cell value in table data element
+            cell.text(value);
+        });
     });
-
-});
+};
