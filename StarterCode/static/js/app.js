ece7e099afd4d93f114ab062fc835afbff5f49e1
Kirsteen Lugtu <klugtu@Kirsteens-MacBook-Pro.local>
2021-12-08 22:02:18 -0600

    Modified app.js so that it filters and loads with blank or non blank filter values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,8 @@
 console.log("app.js loaded")
 
 // from data.js
-var tableData = data;
-console.log(tableData);
+var ufoData = data;
+console.log(ufoData);
 
 // Select button and form
 let button = d3.select("#filter-btn");
@@ -12,30 +12,79 @@ let form = d3.select("#form");
 button.on("click", runFilter);
 form.on("submit", runFilter);
 
-
 function runFilter(){
 
     d3.event.preventDefault();
 
     // ------ Filter out data ---------------
-    let inputElement = d3.select("#datetime");
 
+    let filterObject = {};
+
+    // filter by date
+    let inputElement = d3.select("#datetime");
     let dateIn = inputElement.property("value");
 
-    console.log(dateIn);
+    if (dateIn !== "") {
+        filterObject.datetime = dateIn;
+    }
+    // console.log(dateIn);
+    // let filteredData = ufoData.filter(sighting => sighting.datetime === dateIn);
 
-    let filteredData = tableData.filter(sighting => sighting.datetime === dateIn);
+    // filter by city
+    let cityInputElement = d3.select("#city");
+    let cityIn = cityInputElement.property("value");
+    console.log(cityIn);
 
+    if (cityIn !== "") { 
+       filterObject.city = cityIn;
+       
+    }
+    
+    // // filter by state
+    let stateInputElement = d3.select("#state");
+    let stateIn = stateInputElement.property("value");
+    if (stateIn !== "") {
+        filterObject.state = stateIn;
+    }
+
+    // // filter by country
+    let countryInputElement = d3.select("#country");
+    let countryIn = countryInputElement.property("value");
+    if (countryIn !== "") {
+        filterObject.country = countryIn;
+    }
+
+    // // filter by shape
+    let shapeInputElement = d3.select("#shape");
+    let shapeIn = shapeInputElement.property("value");
+    if (shapeIn !== "") {
+        filterObject.shape = shapeIn;
+    }
+   
+    console.log("showing filter object");
+    console.log(filterObject);
+
+
+    let filteredData = ufoData;
+
+
+    Object.entries(filterObject).forEach(([key, value]) => {
+        filteredData = filteredData.filter(o => o[key] === value);
+
+    });
     console.log(filteredData);
 
-
     // ------ Fill in table with selected data ------
+    buildTable(filteredData);
+    
+};
 
+// build table function
+function buildTable(data) {
     tbody = d3.select("tbody");
+    tbody.html("");
 
-    filteredData.forEach(function(sighting){
-
-        console.log(sighting);
+    data.forEach(function(sighting){
         
         // Add a new row for each ufo sighting
         let row = tbody.append("tr");
@@ -52,4 +101,6 @@ function runFilter(){
             cell.text(value);
         });
     });
-};
+}
+
+buildTable(ufoData);
